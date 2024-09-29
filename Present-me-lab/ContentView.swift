e7df1176935b62c50f968b23f1cd37280646948f
Walghamdi97 <wejgh1997@gmail.com>
2024-09-29 10:54:32 +0300

    New Update

--- Present-me-lab/ContentView.swift
+++ Present-me-lab/ContentView.swift
@@ -10,11 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            //This is for Day 14
+            Image("lilac")
+                .resizable()
+                .frame(width: 300, height: 400)
+                .foregroundColor(.blue)
+                .clipShape(Circle())
+                .overlay {
+                    Circle()
+                      .stroke(.black, lineWidth: 3)
+                  }
+             
+           
+                
+            
+            VStack(alignment: .leading){
+                Text("Wejdan Alghamdi")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                Text("An intellectually curious, rational, and self motivated computer science graduate. Hold a professional accreditaion membership from the SCE. Pationate for cloud computing, web development, and artificial intelligence. Skilled in machine learning, image processing, and web development. ")}
+            
+           
+                        }
         .padding()
     }
 }
