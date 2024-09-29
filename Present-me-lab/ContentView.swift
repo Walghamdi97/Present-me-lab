19a59074858cad3739bae302acecd47520bebdf0
Walghamdi97 <wejgh1997@gmail.com>
2024-09-29 09:30:26 +0300

    Initial Commit

---
+++ Present-me-lab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Present-me-lab
+//
+//  Created by Wejdan Alghamdi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Present-me-lab/Present_me_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  Present_me_labApp.swift
+//  Present-me-lab
+//
+//  Created by Wejdan Alghamdi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Present_me_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
